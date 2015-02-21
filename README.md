73c30eb2f5993ab14ef6ae006ecc4db7b9101046
Patrick Cher <patrickcher@gmail.com>
2015-02-21 23:14:10 +0800

    Revisions to codes
    - Corrected comments to follow syntax conventions
    - Added comments
    - Updated README.md to reflect correct steps for running run_analysis.R
    - Added codes to automatically download dataset to make running of
    codes easier

--- README.md
+++ README.md
@@ -1,13 +1,11 @@
 Getting and Cleaning Data
 =========================
 ## Course Project Read Me
-* Download and unzip the dataset (https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip) into a folder on your local drive. E.g. C:/Users/yourname/Documents/R/Course Project/
-
-* Place run_analysis.R into C:/Users/yourname/Documents/R/Course Project/UCI HAR Dataset/
+* Place run_analysis.R into a folder on your local drive. E.g. C:/Users/yourname/Documents/R/Course Project/
 
 * In RStudio, enter the following commands into the console.
 ````
-setwd("C:/Users/yourname/Documents/R/Course Project\UCI HAR Dataset/")
+setwd("C:/Users/yourname/Documents/R/Course Project/")
 source("run_analysis.R")
 ````
 
@@ -15,3 +13,5 @@ source("run_analysis.R")
 ````
 data <- read.table("tidy_data_with_averages.txt")
 ````
+
+* Note: Do not create UCI HAR Dataset folder manually. This will be created when you source run_analysis.R in console.
--- run_analysis.R
+++ run_analysis.R
@@ -1,57 +1,67 @@
-## This script prepares tidy data that can be used for later analysis
+# This script downloads and prepares tidy UCI HAR data that can be used for later analysis
 
-## 1 - Merges the training and the test sets to create one data set.
-tmp_x_train <- read.table("train/X_train.txt")
-tmp_x_test <- read.table("test/X_test.txt")
+# Step 0 - Download and unzip UCI_HAR_data.zip.
+# NOTE: This will only download data when UCI HAR Dataset folder doesnt exist.
+# If download fails, retry after deleting UCI HAR Dataset folder.
+if (!file.exists("UCI HAR Dataset")) {
+  # download the data
+  fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+  zipfile="UCI_HAR_data.zip"
+  message("Downloading data")
+  download.file(fileURL, destfile=zipfile, method="curl")
+  unzip(zipfile)
+}
+
+# Step 1 - Merges the training and the test sets to create one data set.
+tmp_x_train <- read.table("UCI HAR Dataset/train/X_train.txt")
+tmp_x_test <- read.table("UCI HAR Dataset/test/X_test.txt")
+tmp_s_train <- read.table("UCI HAR Dataset/train/subject_train.txt")
+tmp_s_test <- read.table("UCI HAR Dataset/test/subject_test.txt")
+tmp_y_train <- read.table("UCI HAR Dataset/train/y_train.txt")
+tmp_y_test <- read.table("UCI HAR Dataset/test/y_test.txt")
+
 X_data <- rbind(tmp_x_train, tmp_x_test)
-
-tmp_s_train <- read.table("train/subject_train.txt")
-tmp_s_test <- read.table("test/subject_test.txt")
 S_data <- rbind(tmp_s_train, tmp_s_test)
-
-tmp_y_train <- read.table("train/y_train.txt")
-tmp_y_test <- read.table("test/y_test.txt")
 Y_data <- rbind(tmp_y_train, tmp_y_test)
 
-## 2 - Extracts only the measurements on the mean and 
-## standard deviation for each measurement.
-features <- read.table("features.txt")
+# 2 - Extracts only the measurements on the mean and 
+# standard deviation for each measurement.
+features <- read.table("UCI HAR Dataset/features.txt")
 feature_index <- grep("-mean\\(\\)|-std\\(\\)", features[, 2])
 X_data <- X_data[, feature_index]
 names(X_data) <- features[feature_index, 2]
-## remove brackets and convert to lowercase for uniformity
+# data scrubbing - remove brackets and convert to lowercase for uniformity
 names(X_data) <- gsub("\\(|\\)", "", names(X_data))
 names(X_data) <- tolower(names(X_data))
 
-## 3 - Uses descriptive activity names to name the activities in the data set
-activities <- read.table("activity_labels.txt")
-## remove underscores and convert activity names to lowercase for uniformity
+# Step 3 - Uses descriptive activity names to name the activities in the data set
+activities <- read.table("UCI HAR Dataset/activity_labels.txt")
+# data scrubbing - remove underscores and convert activity names to lowercase for uniformity
 activities[, 2] = gsub("_", "", tolower(as.character(activities[, 2])))
-Y_data[,1] = activities[Y_data[,1], 2]
+Y_data[, 1] = activities[Y_data[, 1], 2]
 names(Y_data) <- "activity"
 
-## 4 - Appropriately labels the data set with descriptive variable names. 
+# Step 4 - Appropriately labels the data set with descriptive variable names. 
 names(S_data) <- "subject"
 cleaned <- cbind(S_data, Y_data, X_data)
 write.table(cleaned, "merged_tidy_data.txt")
 
-## 5 - From the data set in step 4, 
-## creates a second, independent tidy data set 
-## with the average of each variable for each activity and each subject.
-uniqueSubjects = unique(S_data)[,1]
-numOfSubjects = length(unique(S_data)[,1])
-numOfActivities = length(activities[,1])
+# Step 5 - From the data set in step 4, 
+# creates a second, independent tidy data set 
+# with the average of each variable for each activity and each subject.
+uniqueSubjects = unique(S_data)[, 1]
+numOfSubjects = length(unique(S_data)[, 1])
+numOfActivities = length(activities[, 1])
 numOfCols = dim(cleaned)[2]
 result = cleaned[1:(numOfSubjects*numOfActivities), ]
 
 row = 1
-
-for (s in 1:numOfSubjects) {
-  for (a in 1:numOfActivities) {
-    result[row, 1] = uniqueSubjects[s]
-    result[row, 2] = activities[a, 2]
-    tmp <- cleaned[cleaned$subject==s & cleaned$activity==activities[a, 2], ]
-    result[row, 3:numOfCols] <- colMeans(tmp[, 3:numCols])
+for (sub in 1:numOfSubjects) {
+  for (act in 1:numOfActivities) {
+    result[row, 1] = uniqueSubjects[sub]
+    result[row, 2] = activities[act, 2]
+    tmp <- cleaned[cleaned$subject==sub & cleaned$activity==activities[act, 2], ]
+    result[row, 3:numOfCols] <- colMeans(tmp[, 3:numOfCols])
     row = row+1
   }
 }
